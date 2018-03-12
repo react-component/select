566bdc5116b9b9c03fb86fa45cc00ff6b147b91f
Filipe, Cláudia (Developer) <claudia.filipe@sky.uk>
2018-03-12 10:20:19 +0000

    getValueByInput

--- src/Select.jsx
+++ src/Select.jsx
@@ -73,6 +73,7 @@ export default class Select extends React.Component {
     notFoundContent: 'Not Found',
     backfill: false,
     showAction: ['click'],
+    tokenSeparators: [],
   };
 
   constructor(props) {
@@ -166,10 +167,10 @@ export default class Select extends React.Component {
     const val = event.target.value;
     if (
       isMultipleOrTags(this.props) &&
-      tokenSeparators &&
+      tokenSeparators.length &&
       includesSeparators(val, tokenSeparators)
     ) {
-      const nextValue = this.tokenize(val);
+      const nextValue = this.getValueByInput(val);
       this.fireChange(nextValue);
       this.setOpenState(false, true);
       this.setInputValue('', false);
@@ -389,14 +390,8 @@ export default class Select extends React.Component {
         // why not use setState?
         this.state.inputValue = this.getInputDOMNode().value = '';
 
-        if (findIndexInValueByLabel(value, inputValue) === -1) {
-          value = [...value, {
-            key: inputValue,
-            label: inputValue,
-          }];
-          this.fireChange(value);
-          this.fireSelect(value);
-        }
+        value = this.getValueByInput(inputValue);
+        this.fireChange(value);
       }
       props.onBlur(this.getVLForOnChange(value));
       this.setOpenState(false);
@@ -678,6 +673,30 @@ export default class Select extends React.Component {
     }
   };
 
+  getValueByInput = string => {
+    const { multiple, tokenSeparators, children } = this.props;
+    let nextValue = this.state.value;
+    splitBySeparators(string, tokenSeparators).forEach(label => {
+      const selectedValue = { key: label, label };
+      if (findIndexInValueByLabel(nextValue, label) === -1) {
+        if (multiple) {
+          const value = this.getValueByLabel(children, label);
+          if (value) {
+            selectedValue.key = value;
+            nextValue = nextValue.concat(selectedValue);
+          }
+        } else {
+          nextValue = nextValue.concat(selectedValue);
+        }
+      }
+      this.fireSelect({
+        key: label,
+        label,
+      });
+    });
+    return nextValue;
+  };
+
   focus() {
     if (isSingleMode(this.props)) {
       this.selectionRef.focus();
@@ -893,30 +912,6 @@ export default class Select extends React.Component {
       const childValue = getValuePropValue(child);
       return childValue === key && child.props && child.props.disabled;
     });
-  };
-
-  tokenize = string => {
-    const { multiple, tokenSeparators, children } = this.props;
-    let nextValue = this.state.value;
-    splitBySeparators(string, tokenSeparators).forEach(label => {
-      const selectedValue = { key: label, label };
-      if (findIndexInValueByLabel(nextValue, label) === -1) {
-        if (multiple) {
-          const value = this.getValueByLabel(children, label);
-          if (value) {
-            selectedValue.key = value;
-            nextValue = nextValue.concat(selectedValue);
-          }
-        } else {
-          nextValue = nextValue.concat(selectedValue);
-        }
-      }
-      this.fireSelect({
-        key: label,
-        label,
-      });
-    });
-    return nextValue;
   };
 
   adjustOpenState = () => {
