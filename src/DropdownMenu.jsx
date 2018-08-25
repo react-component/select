95ad0d91c89457bc8f97a4f2c916c852226c5aca
Islam Attrash <isattrash@gmail.com>
2018-08-25 07:32:22 +0300

    [enhancement] - Scrolling performance issues (#302)
    * [enhancement] - Scrolling performance issues
    
    This commit will resolve a known scrolling performance issue related to scrollable areas.
    The thing is that scrolling force repaints (draw the UI pixels) whenever the user is scrolling,
    and that causes a lot of repaints that could be avoided using GPU accelerated repaints.
    
    For legacy fix I'm using transform: translateZ(0) which will give the GPU to handle those repaints which make
    the scrolling performance more better
    
    Affected pages among the examples with this issue:
    - http://react-component.github.io/select/examples/tags.html
    - http://react-component.github.io/select/examples/single.html
    - http://react-component.github.io/select/examples/multiple.html
    
    Inspect the Chrome "rendering" tab in Devtools to validate the issue.
    I'll be attaching a screenshots for the reproduction
    
    * Fixes for Test snapshots

--- src/DropdownMenu.jsx
+++ src/DropdownMenu.jsx
@@ -179,7 +179,10 @@ export default class DropdownMenu extends React.Component {
     const renderMenu = this.renderMenu();
     return renderMenu ? (
       <div
-        style={{ overflow: 'auto' }}
+        style={{
+          overflow: 'auto',
+          transform: 'translateZ(0)',
+        }}
         onFocus={this.props.onPopupFocus}
         onMouseDown={preventDefaultEvent}
         onScroll={this.props.onPopupScroll}
