7874f3c9d28626e3173b96e5faf3324c7a250ad7
zombiej <smith3816@gmail.com>
2018-09-20 15:30:30 +0800

    update test case

--- src/Select.jsx
+++ src/Select.jsx
@@ -1067,7 +1067,7 @@ class Select extends React.Component {
         }
 
         // Match option group label
-        if (this.filterOption(inputValue, child)) {
+        if (inputValue && this.filterOption(inputValue, child)) {
           const innerItems = childrenToArray(child.props.children).map((subChild) => {
             const childValue = getValuePropValue(subChild);
             return (
