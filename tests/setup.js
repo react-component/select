28898304d5822af1b41400aafde88cb869cfd3ba
林哲源 <linzheyuan@ebaolife.com>
2018-09-27 16:52:45 +0800

    use 'raf'

--- tests/setup.js
+++ tests/setup.js
@@ -1,34 +1,4 @@
-(function polyfill() {
-  let lastTime = 0;
-  const vendors = ['ms', 'moz', 'webkit', 'o'];
-  let x;
-  let length;
-  let currTime;
-  let timeToCall;
-
-  for (x = 0, length = vendors.length; x < length && !window.requestAnimationFrame; ++x) {
-    window.requestAnimationFrame = window[`${vendors[x]}RequestAnimationFrame`];
-    window.cancelAnimationFrame =
-    window[`${vendors[x]}CancelAnimationFrame`] ||
-    window[`${vendors[x]}CancelRequestAnimationFrame`];
-  }
-
-  if (!window.requestAnimationFrame) {
-    window.requestAnimationFrame = function requestAnimationFrame(callback) {
-      currTime = new Date().getTime();
-      timeToCall = Math.max(0, 16 - (currTime - lastTime));
-      lastTime = currTime + timeToCall;
-      return window.setTimeout(() => { callback(currTime + timeToCall); },
-        timeToCall);
-    };
-  }
-
-  if (!window.cancelAnimationFrame) {
-    window.cancelAnimationFrame = function cancelAnimationFrame(id) {
-      clearTimeout(id);
-    };
-  }
-}());
+require('raf').polyfill();
 
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
