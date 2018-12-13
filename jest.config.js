420bbdfc9c336e09a13b64e40714ce006d33fd7f
陈帅 <qixian.cs@outlook.com>
2018-12-13 12:13:57 +0800

    config:migrate jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,16 +2,11 @@ module.exports = {
   verbose: true,
   setupFiles: ['./tests/setup.ts'],
   testPathIgnorePatterns: ['/node_modules/', 'dekko', 'node'],
-  transform: {
-    '\\.ts$': 'ts-jest',
-    '\\.jsx?$': './node_modules/rc-tools/scripts/jestPreprocessor.js',
-  },
   modulePathIgnorePatterns: ['/examples/'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   preset: 'ts-jest',
   globals: {
     'ts-jest': {
-      tsConfigFile: './tsconfig.test.json',
+      tsConfig: './tsconfig.test.json',
     },
   },
   snapshotSerializers: ['enzyme-to-json/serializer'],
