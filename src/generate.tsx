14e0a59ed142ab8193a9b67c87a1f7bdd1f0517f
二货机器人 <smith3816@gmail.com>
2020-02-02 00:28:18 +0800

    fix: select reset logic (#454)

--- src/generate.tsx
+++ src/generate.tsx
@@ -380,7 +380,10 @@ export default function generateSelector<
     // Should reset when controlled to be uncontrolled
     const prevValueRef = React.useRef(value);
     React.useEffect(() => {
-      if (prevValueRef.current !== value && value === undefined) {
+      if (
+        prevValueRef.current !== value &&
+        (value === undefined || value === null)
+      ) {
         setInnerValue(undefined);
       }
       prevValueRef.current = value;
