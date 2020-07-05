ed63549d8cef1e728057b7a55ecabaf61fff7b96
偏右 <afc163@gmail.com>
2020-07-05 21:35:23 +0800

    fix onFocus not trigger in IE11 (#513)
    * fix onFocus not trigger in IE11
    
    close https://github.com/ant-design/ant-design/issues/25354
    
    * use IE hacks
    
    * fix test case

--- src/Selector/index.tsx
+++ src/Selector/index.tsx
@@ -196,18 +196,21 @@ const Selector: React.RefForwardingComponent<RefSelectorProps, SelectorProps> = 
     pastedTextRef.current = value;
   };
 
-  // ====================== Focus ======================
-  // Should focus input if click the selector
-  const onClick = ({ target }) => {
-    if (target !== inputRef.current) {
-      inputRef.current.focus();
-    }
-  };
-
   const onMouseDown: React.MouseEventHandler<HTMLElement> = event => {
     const inputMouseDown = getInputMouseDown();
-    if (event.target !== inputRef.current && !inputMouseDown) {
-      event.preventDefault();
+    if (event.target !== inputRef.current) {
+      if (!inputMouseDown) {
+        event.preventDefault();
+      }
+      // Should focus input if click the selector
+      const isIE = (document.body.style as any).msTouchAction !== undefined;
+      if (isIE) {
+        setTimeout(() => {
+          inputRef.current.focus();
+        });
+      } else {
+        inputRef.current.focus();
+      }
     }
 
     if ((mode !== 'combobox' && (!showSearch || !inputMouseDown)) || !open) {
@@ -236,12 +239,7 @@ const Selector: React.RefForwardingComponent<RefSelectorProps, SelectorProps> = 
   );
 
   return (
-    <div
-      ref={domRef}
-      className={`${prefixCls}-selector`}
-      onClick={onClick}
-      onMouseDown={onMouseDown}
-    >
+    <div ref={domRef} className={`${prefixCls}-selector`} onMouseDown={onMouseDown}>
       {selectNode}
     </div>
   );
