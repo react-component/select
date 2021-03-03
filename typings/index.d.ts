307786a222355ea87de0a2f51ea581334c9c0075
二货机器人 <smith3816@gmail.com>
2021-03-03 17:54:15 +0800

    chore: Select without combobox mode should always not submit form (#604)
    * fix: Not submit when enter
    
    * test: Add test case

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -6,8 +6,6 @@ declare module 'rc-menu';
 
 declare module 'rc-util/lib/Children/toArray';
 
-declare module 'rc-util/lib/KeyCode';
-
 declare module 'dom-scroll-into-view';
 
 declare module 'rc-trigger';
