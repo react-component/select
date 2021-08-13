0bdc462e3a0086fb5f279a2f8dce1a3b632d9cc9
wendellhu <wendellhu@tencent.com>
2021-08-13 13:50:07 +0800

    fix: fix mac platform on Firefox

--- src/utils/platformUtil.ts
+++ src/utils/platformUtil.ts
@@ -1,4 +1,4 @@
 /* istanbul ignore file */
 export function isPlatformMac(): boolean {
-  return /mac\sos/i.test(navigator.appVersion);
+  return /(mac\sos|macintosh)/i.test(navigator.appVersion);
 }
