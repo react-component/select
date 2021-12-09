fdc3410cd24bda6c2479cf5f24fe8f6e399bf9d3
zombiej <smith3816@gmail.com>
2021-12-09 11:04:21 +0800

    chore: export BaseSelectRef

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import Option from './Option';
 import OptGroup from './OptGroup';
 import type { SelectProps } from './Select';
 import BaseSelect from './BaseSelect';
-import type { BaseSelectProps } from './BaseSelect';
+import type { BaseSelectProps, BaseSelectRef } from './BaseSelect';
 import useBaseProps from './hooks/useBaseProps';
 
 export { Option, OptGroup, BaseSelect, useBaseProps };
-export type { SelectProps, BaseSelectProps };
+export type { SelectProps, BaseSelectProps, BaseSelectRef };
 
 export default Select;
