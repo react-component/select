a64b6baca73ec84551c7eb65fdddb6d178107352
二货爱吃白萝卜 <smith3816@gmail.com>
2023-08-23 17:33:10 +0800

    refactor: Select use `@rc-component/trigger` align logic instead of self measure (#976)
    * chore: use trigger stretch
    
    * test: update snapshot

--- src/SelectTrigger.tsx
+++ src/SelectTrigger.tsx
@@ -62,7 +62,6 @@ export interface SelectTriggerProps {
 
   animation?: string;
   transitionName?: string;
-  containerWidth: number;
   placement?: Placement;
   builtinPlacements?: BuildInPlacements;
   dropdownStyle: React.CSSProperties;
@@ -90,7 +89,6 @@ const SelectTrigger: React.RefForwardingComponent<RefTriggerProps, SelectTrigger
     visible,
     children,
     popupElement,
-    containerWidth,
     animation,
     transitionName,
     dropdownStyle,
@@ -124,23 +122,32 @@ const SelectTrigger: React.RefForwardingComponent<RefTriggerProps, SelectTrigger
   // ===================== Motion ======================
   const mergedTransitionName = animation ? `${dropdownPrefixCls}-${animation}` : transitionName;
 
+  // =================== Popup Width ===================
+  const isNumberPopupWidth = typeof dropdownMatchSelectWidth === 'number';
+
+  const stretch = React.useMemo(() => {
+    if (isNumberPopupWidth) {
+      return null;
+    }
+
+    return dropdownMatchSelectWidth === false ? 'minWidth' : 'width';
+  }, [dropdownMatchSelectWidth, isNumberPopupWidth]);
+
+  let popupStyle = dropdownStyle;
+
+  if (isNumberPopupWidth) {
+    popupStyle = {
+      ...popupStyle,
+      width: dropdownMatchSelectWidth,
+    };
+  }
+
   // ======================= Ref =======================
   const popupRef = React.useRef<HTMLDivElement>(null);
 
   React.useImperativeHandle(ref, () => ({
     getPopupElement: () => popupRef.current,
   }));
-
-  const popupStyle: React.CSSProperties = {
-    minWidth: containerWidth,
-    ...dropdownStyle,
-  };
-
-  if (typeof dropdownMatchSelectWidth === 'number') {
-    popupStyle.width = dropdownMatchSelectWidth;
-  } else if (dropdownMatchSelectWidth) {
-    popupStyle.width = containerWidth;
-  }
 
   return (
     <Trigger
@@ -156,6 +163,7 @@ const SelectTrigger: React.RefForwardingComponent<RefTriggerProps, SelectTrigger
           {popupNode}
         </div>
       }
+      stretch={stretch}
       popupAlign={dropdownAlign}
       popupVisible={visible}
       getPopupContainer={getPopupContainer}
