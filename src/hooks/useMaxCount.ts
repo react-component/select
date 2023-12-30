7c4f99cdbc99c3e775b9d5688e0549ab0ac7ee43
栗嘉男 <574980606@qq.com>
2023-12-31 05:17:37 +0800

    fix: fix

--- src/hooks/useMaxCount.ts
+++ src/hooks/useMaxCount.ts
@@ -3,23 +3,21 @@ import SelectContext from '../SelectContext';
 import type { SelectContextProps } from '../SelectContext';
 
 const useMaxCount = (multiple: boolean) => {
-  const { maxCount, rawValues } = React.useContext<SelectContextProps>(SelectContext);
-  return {
-    shouldTruncate: React.useCallback(
-      (overCount = true) => {
-        if (!multiple || typeof maxCount === 'undefined') {
-          return false;
-        }
-        const overMaxCount = rawValues?.size >= maxCount;
-        return overCount ? overMaxCount : !overMaxCount;
-      },
-      [multiple, maxCount, rawValues?.size],
-    ),
-    truncateLength: React.useMemo<number>(
-      () => maxCount - rawValues?.size,
-      [maxCount, rawValues?.size],
-    ),
-  };
+  const { maxCount, rawValues } = React.useContext<SelectContextProps>(SelectContext) || {};
+  const truncateLength = React.useMemo<number>(
+    () => maxCount - rawValues?.size,
+    [maxCount, rawValues?.size],
+  );
+  const shouldTruncate = React.useCallback(
+    (overCount = true) => {
+      if (!multiple || typeof maxCount === 'undefined') {
+        return false;
+      }
+      return overCount ? truncateLength < 0 : truncateLength >= 0;
+    },
+    [multiple, maxCount, truncateLength],
+  );
+  return { shouldTruncate, truncateLength };
 };
 
 export default useMaxCount;
