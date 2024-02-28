b6ce9805ab9217adb247c0a12824065d4e35766d
叶枫 <645381995@qq.com>
2024-02-28 15:40:29 +0800

    feat: add demo

--- docs/examples/focus.tsx
+++ docs/examples/focus.tsx
@@ -1,26 +1,18 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
 import type { BaseSelectRef } from 'rc-select';
 import Select, { Option } from 'rc-select';
 import '../../assets/index.less';
 
 const MySelect = () => {
   const ref = useRef<BaseSelectRef>(null);
-  // useLayoutEffect(() => {
-  //   if (ref.current) {
-  //     ref.current.focus({ preventScroll: true });
-  //   }
-  // }, []);
-  useEffect(() => {
-    setTimeout(() => {
-      if (ref.current) {
-        console.log('1', ref.current);
-        ref.current.focus({ preventScroll: true });
-      }
-    });
+  useLayoutEffect(() => {
+    if (ref.current) {
+      ref.current.focus({ preventScroll: true });
+    }
   }, []);
   return (
     <div>
-      <Select autoFocus placeholder="placeholder" ref={ref}>
+      <Select placeholder="placeholder" showSearch ref={ref}>
         <Option value="11" text="lucy">
           lucy
         </Option>
@@ -31,24 +23,13 @@ const MySelect = () => {
     </div>
   );
 };
-// const MySelect = () => {
-//   const ref = useRef<HTMLDivElement>(null);
-//   useEffect(() => {
-//     setTimeout(() => {
-//       if (ref.current) {
-//         ref.current.focus();
-//       }
-//     });
-//   }, []);
-//   return <div ref={ref}>哈哈哈哈</div>;
-// };
 
 const Demo = () => {
   const [open, setOpen] = useState(false);
   return (
     <div>
       <div style={{ height: '50vh' }} />
-      <a onClick={() => setOpen(!open)}>open</a>
+      <a onClick={() => setOpen(!open)}>{`${open}`}</a>
       <div style={{ height: '80vh' }} />
       {open && <MySelect />}
       <div style={{ height: '30vh' }} />
