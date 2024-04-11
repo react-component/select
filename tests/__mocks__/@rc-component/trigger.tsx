5058e605eadf0a8c14ab90eeaab5bed46b83173c
二货机器人 <smith3816@gmail.com>
2024-04-11 11:22:19 +0800

    test: fix test case

--- tests/__mocks__/@rc-component/trigger.tsx
+++ tests/__mocks__/@rc-component/trigger.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Trigger from '@rc-component/trigger/lib/mock';
 
-export default (props) => {
+export default React.forwardRef((props, ref) => {
   global.triggerProps = props;
-  return <Trigger {...props} />;
-};
+  return <Trigger {...props} ref={ref} />;
+});
