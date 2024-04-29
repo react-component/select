b0bd721008768ef6a093d552a953fcc8af12025f
叶枫 <645381995@qq.com>
2024-04-29 16:03:33 +0800

    feat: test

---
+++ docs/demo/panel.md
@@ -0,0 +1,8 @@
+---
+title: panel
+nav:
+  title: Demo
+  path: /demo
+---
+
+<code src="../examples/panel.tsx"></code>
---
+++ docs/examples/panel.tsx
@@ -0,0 +1,28 @@
+import Select from 'rc-select';
+import '../../assets/index.less';
+
+export default () => {
+  return (
+    <>
+      {/* <Select
+        options={[
+          { label: 'test1', value: '1' },
+          { label: 'test2', value: '2' },
+        ]}
+      /> */}
+      {/* <Select.SelectPanel
+        open
+        options={[
+          { label: 'test1', value: '1' },
+          { label: 'test2', value: '2' },
+        ]}
+      /> */}
+      <Select
+        options={[
+          { label: 'test1', value: '1' },
+          { label: 'test2', value: '2' },
+        ]}
+      />
+    </>
+  );
+};
