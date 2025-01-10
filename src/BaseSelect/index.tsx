fa18f57028bc5aefb287a8d67d9c4bccc3bf3f67
EmilyyyLiu <100924403+EmilyyyLiu@users.noreply.github.com>
2025-01-10 14:17:32 +0800

    feat：Give the parameter 'info' field 'index' of the renderItem method in rc-overflow to the tagRender method (#1102)
    * feat：从rc-overflow的renderItem中获取info参数，后添加到tagRender中
    
    * 修改不正确的类型定义
    
    ---------
    
    Co-authored-by: 刘欢 <lh01217311@antgroup.com>

--- src/BaseSelect/index.tsx
+++ src/BaseSelect/index.tsx
@@ -68,6 +68,7 @@ export type CustomTagProps = {
   onClose: (event?: React.MouseEvent<HTMLElement, MouseEvent>) => void;
   closable: boolean;
   isMaxTag: boolean;
+  index: number;
 };
 
 export interface BaseSelectRef {
