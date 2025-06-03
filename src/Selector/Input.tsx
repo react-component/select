5dcb2c225ac5c1eee18d3185e6e36a542870127b
𝑾𝒖𝒙𝒉 <wxh16144@qq.com>
2025-06-03 17:57:27 +0800

    fix: simplify input props handling and improve accessibility features (#1150) (#1151)
    * fix: simplify input props handling and improve accessibility features
    
    * chore: add unit test
    
    (cherry picked from commit e6a3ca7f918495a3fdc8f7866c5d8e07941a5799)
    
    # Conflicts:
    #	src/Selector/Input.tsx

--- src/Selector/Input.tsx
+++ src/Selector/Input.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import classNames from 'classnames';
 import { composeRef } from '@rc-component/util/lib/ref';
 import { warning } from '@rc-component/util/lib/warning';
+import composeProps from '@rc-component/util/lib/composeProps';
 import useBaseProps from '../hooks/useBaseProps';
+
 type InputRef = HTMLInputElement | HTMLTextAreaElement;
 
 interface InputProps {
@@ -39,23 +41,13 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (props, ref)
     prefixCls,
     id,
     inputElement,
-    disabled,
-    tabIndex,
-    autoFocus,
     autoComplete,
     editable,
     activeDescendantId,
     value,
-    maxLength,
-    onKeyDown,
-    onMouseDown,
-    onChange,
-    onPaste,
-    onCompositionStart,
-    onCompositionEnd,
-    onBlur,
     open,
     attrs,
+    ...restProps
   } = props;
 
   const { classNames: contextClassNames, styles: contextStyles } = useBaseProps() || {};
@@ -64,16 +56,6 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (props, ref)
 
   const { ref: originRef, props: originProps } = inputNode;
 
-  const {
-    onKeyDown: onOriginKeyDown,
-    onChange: onOriginChange,
-    onMouseDown: onOriginMouseDown,
-    onCompositionStart: onOriginCompositionStart,
-    onCompositionEnd: onOriginCompositionEnd,
-    onBlur: onOriginBlur,
-    style,
-  } = originProps;
-
   warning(
     !('maxLength' in inputNode.props),
     `Passing 'maxLength' to input element directly may not work because input in BaseSelect is controlled.`,
@@ -81,18 +63,16 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (props, ref)
 
   inputNode = React.cloneElement(inputNode, {
     type: 'search',
-    ...originProps,
+    ...composeProps(restProps, originProps, true),
+
     // Override over origin props
     id,
     ref: composeRef(ref, originRef as any),
-    disabled,
-    tabIndex,
     autoComplete: autoComplete || 'off',
 
-    autoFocus,
     className: classNames(
       `${prefixCls}-selection-search-input`,
-      inputNode?.props?.className,
+      originProps.className,
       contextClassNames?.input,
     ),
 
@@ -105,48 +85,13 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (props, ref)
     'aria-activedescendant': open ? activeDescendantId : undefined,
     ...attrs,
     value: editable ? value : '',
-    maxLength,
     readOnly: !editable,
     unselectable: !editable ? 'on' : null,
 
-    style: { ...style, opacity: editable ? null : 0, ...contextStyles?.input },
-
-    onKeyDown: (event: React.KeyboardEvent<HTMLElement>) => {
-      onKeyDown(event);
-      if (onOriginKeyDown) {
-        onOriginKeyDown(event);
-      }
-    },
-    onMouseDown: (event: React.MouseEvent<HTMLElement>) => {
-      onMouseDown(event);
-      if (onOriginMouseDown) {
-        onOriginMouseDown(event);
-      }
-    },
-    onChange: (event: React.ChangeEvent<HTMLElement>) => {
-      onChange(event);
-      if (onOriginChange) {
-        onOriginChange(event);
-      }
-    },
-    onCompositionStart(event: React.CompositionEvent<HTMLElement>) {
-      onCompositionStart(event);
-      if (onOriginCompositionStart) {
-        onOriginCompositionStart(event);
-      }
-    },
-    onCompositionEnd(event: React.CompositionEvent<HTMLElement>) {
-      onCompositionEnd(event);
-      if (onOriginCompositionEnd) {
-        onOriginCompositionEnd(event);
-      }
-    },
-    onPaste,
-    onBlur(event: React.FocusEvent<HTMLElement>) {
-      onBlur(event);
-      if (onOriginBlur) {
-        onOriginBlur(event);
-      }
+    style: {
+      ...originProps.style,
+      opacity: editable ? null : 0,
+      ...contextStyles?.input,
     },
   });
 
