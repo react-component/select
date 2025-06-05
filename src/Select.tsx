ec0b918a1b28b990417b52edc6867857d9c623bf
刘欢 <lh01217311@antgroup.com>
2025-06-05 11:54:09 +0800

    feat: set autoClearSearchValue default value

--- src/Select.tsx
+++ src/Select.tsx
@@ -238,7 +238,11 @@ const Select = React.forwardRef<BaseSelectRef, SelectProps<any, DefaultOptionTyp
     ];
     const legacyShowSearch: SearchConfig<DefaultOptionType> = {};
     legacySearchProps.forEach((propsName) => {
-      legacyShowSearch[propsName] = restProps?.[propsName];
+      if (propsName === 'autoClearSearchValue') {
+        legacyShowSearch[propsName] = props?.[propsName] ?? true;
+        return;
+      }
+      legacyShowSearch[propsName] = props?.[propsName];
     });
     const mergedShowSearch = typeof showSearch === 'object' ? showSearch : legacyShowSearch;
     const {
@@ -248,7 +252,7 @@ const Select = React.forwardRef<BaseSelectRef, SelectProps<any, DefaultOptionTyp
       optionLabelProp,
       filterSort,
       onSearch,
-      autoClearSearchValue,
+      autoClearSearchValue = true,
       tokenSeparators,
     } = mergedShowSearch;
 
