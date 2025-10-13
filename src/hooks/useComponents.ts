04db27558cdf3cdb4a6e7b48737411451c043c9b
二货机器人 <smith3816@gmail.com>
2025-10-13 17:31:42 +0800

    chore: update

--- src/hooks/useComponents.ts
+++ src/hooks/useComponents.ts
@@ -3,16 +3,18 @@ import SelectInput, { type SelectInputProps } from '../SelectInput';
 
 export interface ComponentsConfig {
   root?: React.ComponentType<any>;
+  input?: React.ComponentType<any>;
 }
 
 export interface ReturnType {
-  root: React.ComponentType<SelectInputProps>;
+  root: React.ComponentType<SelectInputProps> | string;
+  input: React.ComponentType<any> | string;
 }
 
 export default function useComponents(components?: ComponentsConfig): ReturnType {
   return React.useMemo(() => {
-    const { root: RootComponent = SelectInput } = components || {};
+    const { root: RootComponent = SelectInput, input: InputComponent = 'input' } = components || {};
 
-    return { root: RootComponent };
+    return { root: RootComponent, input: InputComponent };
   }, [components]);
 }
