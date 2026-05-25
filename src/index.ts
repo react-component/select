b0b527bbe920a9b58214b5bb3ebdba3db7af8dd8
高艳兵 <yanbing.gao@aliyun.com>
2026-05-25 13:29:45 +0800

    fix: trim select type exports

--- src/index.ts
+++ src/index.ts
@@ -3,33 +3,12 @@ import Option from './Option';
 import OptGroup from './OptGroup';
 import type { BaseOptionType, DefaultOptionType, SearchConfig, SelectProps } from './Select';
 import BaseSelect from './BaseSelect';
-import type {
-  BaseSelectProps,
-  BaseSelectPropsWithoutPrivate,
-  BaseSelectRef,
-  BaseSelectSemanticName,
-  DisplayValueType,
-  RefOptionListProps,
-} from './BaseSelect';
+import type { BaseSelectProps, BaseSelectRef, BaseSelectPropsWithoutPrivate } from './BaseSelect';
 import useBaseProps from './hooks/useBaseProps';
-import type { OptionFC, OptionProps } from './Option';
-import type { Placement } from './interface';
+import type { OptionProps } from './Option';
 
 export { Option, OptGroup, BaseSelect, useBaseProps };
-export type {
-  BaseOptionType,
-  BaseSelectProps,
-  BaseSelectPropsWithoutPrivate,
-  BaseSelectRef,
-  BaseSelectSemanticName,
-  DefaultOptionType,
-  DisplayValueType,
-  OptionFC,
-  OptionProps,
-  Placement,
-  RefOptionListProps,
-  SearchConfig,
-  SelectProps,
-};
+export type { BaseOptionType, DefaultOptionType, OptionProps, SearchConfig };
+export type { SelectProps, BaseSelectProps, BaseSelectRef, BaseSelectPropsWithoutPrivate };
 
 export default Select;
